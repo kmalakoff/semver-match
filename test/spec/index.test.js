473d3f46eee3f0957f0d886b1fb7799e6de80722
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 21:57:36 -0800

    Upgrade modules

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -1,52 +1,52 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var match = require('../..');
+const match = require('match-semver');
 
-describe('match', function () {
-  describe('comparators', function () {
-    it('eq', function () {
+describe('match', () => {
+  describe('comparators', () => {
+    it('eq', () => {
       assert.ok(!match('v1.0.0', { eq: 'v0.0.0' }));
       assert.ok(match('v1.0.0', { eq: 'v1.0.0' }));
       assert.ok(!match('v1.0.0', { eq: 'v2.0.0' }));
     });
-    it('lt', function () {
+    it('lt', () => {
       assert.ok(!match('v1.0.0', { lt: 'v0.0.0' }));
       assert.ok(!match('v1.0.0', { lt: 'v1.0.0' }));
       assert.ok(match('v1.0.0', { lt: 'v2.0.0' }));
     });
-    it('lte', function () {
+    it('lte', () => {
       assert.ok(!match('v1.0.0', { lte: 'v0.0.0' }));
       assert.ok(match('v1.0.0', { lte: 'v1.0.0' }));
       assert.ok(match('v1.0.0', { lte: 'v2.0.0' }));
     });
-    it('gt', function () {
+    it('gt', () => {
       assert.ok(match('v1.0.0', { gt: 'v0.0.0' }));
       assert.ok(!match('v1.0.0', { gt: 'v1.0.0' }));
       assert.ok(!match('v1.0.0', { gt: 'v2.0.0' }));
     });
-    it('gte', function () {
+    it('gte', () => {
       assert.ok(match('v1.0.0', { gte: 'v0.0.0' }));
       assert.ok(match('v1.0.0', { gte: 'v1.0.0' }));
       assert.ok(!match('v1.0.0', { gte: 'v2.0.0' }));
     });
   });
-  describe('ranges', function () {
-    it('gte,lt', function () {
+  describe('ranges', () => {
+    it('gte,lt', () => {
       assert.ok(!match('v1.0.0', { gte: 'v0.0.0', lt: 'v1.0.0' }));
       assert.ok(match('v1.0.0', { gte: 'v1.0.0', lt: 'v2.0.0' }));
       assert.ok(match('v1.0.0', { gte: 'v0.0.0', lt: 'v2.0.0' }));
     });
-    it('gte,lte', function () {
+    it('gte,lte', () => {
       assert.ok(match('v1.0.0', { gte: 'v0.0.0', lte: 'v1.0.0' }));
       assert.ok(match('v1.0.0', { gte: 'v1.0.0', lte: 'v2.0.0' }));
       assert.ok(match('v1.0.0', { gte: 'v0.0.0', lte: 'v2.0.0' }));
     });
-    it('gt,lt', function () {
+    it('gt,lt', () => {
       assert.ok(!match('v1.0.0', { gt: 'v0.0.0', lt: 'v1.0.0' }));
       assert.ok(!match('v1.0.0', { gt: 'v1.0.0', lt: 'v2.0.0' }));
       assert.ok(match('v1.0.0', { gt: 'v0.0.0', lt: 'v2.0.0' }));
     });
-    it('gt,lte', function () {
+    it('gt,lte', () => {
       assert.ok(match('v1.0.0', { gt: 'v0.0.0', lte: 'v1.0.0' }));
       assert.ok(!match('v1.0.0', { gt: 'v1.0.0', lte: 'v2.0.0' }));
       assert.ok(match('v1.0.0', { gt: 'v0.0.0', lte: 'v2.0.0' }));
